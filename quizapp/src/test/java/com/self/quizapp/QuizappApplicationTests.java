c9559ba771b2731dfbb19643e8e0cb96029f5850
Saqib <saqib29111@gmail.com>
2021-09-03 23:31:23 +0600

    fresh project start with dependecies

---
+++ quizapp/src/test/java/com/self/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
