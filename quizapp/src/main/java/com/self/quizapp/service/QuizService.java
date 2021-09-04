462d52ade5b82a5478d162442c2e279c1b8285bf
Saqib <saqib29111@gmail.com>
2021-09-04 22:45:00 +0600

    basic app to understand

---
+++ quizapp/src/main/java/com/self/quizapp/service/QuizService.java
@@ -0,0 +1,73 @@
+package com.self.quizapp.service;
+
+import com.self.quizapp.model.Question;
+import com.self.quizapp.model.QuestionForm;
+import com.self.quizapp.model.Result;
+import com.self.quizapp.repository.QuestionRepo;
+import com.self.quizapp.repository.ResultRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class QuizService {
+
+    @Autowired
+    Question question;
+
+    @Autowired
+    QuestionForm qForm;
+
+    @Autowired
+    QuestionRepo qRepo;
+
+    @Autowired
+    Result result;
+
+    @Autowired
+    ResultRepo rRepo;
+
+    public QuestionForm getQuestions(){
+        List<Question> allQuestion = qRepo.findAll();
+        List<Question> qList = new ArrayList<Question>();
+
+        Random random = new Random();
+
+        for (int i=0; i<5; i++){
+            int rand = random.nextInt(allQuestion.size());
+            qList.add(allQuestion.get(rand));
+            allQuestion.remove(rand);
+        }
+
+        qForm.setQuestions(qList);
+
+        return qForm;
+    }
+
+    public int getResult(QuestionForm qForm){
+        int correct = 0;
+        for (Question q : qForm.getQuestions()){
+            if (q.getAns() == q.getChose()){
+                correct++;
+            }
+        }
+        return correct;
+    }
+
+    public void saveScore(Result result){
+        Result saveResult = new Result();
+        saveResult.setUsername(result.getUsername());
+        saveResult.setTotalCorrect(result.getTotalCorrect());
+        rRepo.save(saveResult);
+    }
+
+    public List<Result> getTopScore(){
+        List<Result> sList = rRepo.findAll(Sort.by(Sort.Direction.DESC, "totalCorrect"));
+
+        return sList;
+    }
+}
